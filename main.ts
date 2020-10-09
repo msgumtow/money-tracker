5c3502df47740c77af8dbe794e2b87798073df40
msgumtow <71787320+msgumtow@users.noreply.github.com>
2020-10-09 12:46:45 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+/**
+ * When I press button A, the money amount will change by 1. 
+ * 
+ * When I press another button, the money will go down by 1. 
+ * 
+ * When I shake the microbit, it will show the number "money" to display the total amount.
+ */
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
